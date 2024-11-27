fa35c7e229a48bbd8a3871e058c8cfbe453ce9d8
Ong <kaiyin666ong@gmail.com>
2024-11-27 19:03:27 +0800

    change font size function

---
+++ src/Main_frame/QuizResult_frame/QuizResultPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../SideBar/SideBar'
+
+function QuizResultPage() {
+  return (
+    <div>
+        <div>
+            <SideBar/>
+        </div>
+    </div>
+  )
+}
+
+export default QuizResultPage
