b6c8a575e7e02c48b8284df15ef26dd276aaaa17
Ong <kaiyin666ong@gmail.com>
2024-11-27 18:52:57 +0800

    update function

---
+++ src/Main_frame/Dashboard_frame/DashboardPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SideBar from '../SideBar/SideBar'
+
+
+function DashboardPage() {
+  return (
+    <div>
+        <div>
+            <SideBar/>
+        </div>
+        {/* You can start your code here */}
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ src/Main_frame/Profile_frame/ProfilePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from '../SideBar/SideBar'
+
+function ProfilePage() {
+  return (
+    <div>
+        <div>
+            <SideBar/>
+        </div>
+        {/* You can start your code here */}
+    </div>
+  )
+}
+
+export default ProfilePage
