9f161f300d1a34c6193d435fbb167fc3913885da
Ong <kaiyin666ong@gmail.com>
2024-12-11 11:15:51 +0800

    update

--- src/Login_page/Forgot_password/password_page.js
+++ src/Login_page/Forgot_password/password_page.js
@@ -7,9 +7,31 @@ function Password_page  () {
 
   const navigate = useNavigate();
 
+  // navigate to PasswordPage2
   const NavigatetoNext=(e)=>{
     e.preventDefault();
     navigate("/PasswordPage2");
+  }
+
+  const HandleForgetPassword = async (e)=>{
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    try{
+      // send get request to backend 
+      const res = await fetch (`http://127.0.0.1:5000/forgetPassword/send_email?target=${email}`);
+
+      // wait for the response form backend server and Parse the Json body
+      const data = await res.json(); 
+      if(data.message === "Invalid Email"){
+        console.log("Invalid email");
+
+      }else if (data.message === "Application attempted to send email, check CLI for any errors"){
+        // if success navigate to next page 
+        NavigatetoNext(e);
+      }
+    }catch(err){
+      console.error(err);
+    }
   }
 
   return (
@@ -25,7 +47,7 @@ function Password_page  () {
                 <label for="email">Email
                   <input name="email" id="email" required type="email" placeholder="Enter your email"/>
                 </label>
-                <button type="submit" onClick={NavigatetoNext}>Reset password</button>
+                <button type="submit" onClick={(e)=>HandleForgetPassword(e)}>Reset password</button>
               </form>
           </div>
       </div>
